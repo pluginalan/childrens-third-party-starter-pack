46d1839df3b9e962a9c8e531945a2055e8d47a0a
David Birdsall <david.birdsall@bbc.co.uk>
2017-08-01 16:21:47 +0100

    add script to sync the remote directory up to s3

---
+++ remote-sync.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [ -z `which aws` ]; then
+  echo "aws cli can be installed with: brew install awscli"
+  exit 1
+fi
+
+LOCAL="remote"
+REMOTE="s3://childrens-data-public-int/apps/cbeebies-picknmix-dev/remote/"
+PROFILE="childrens-data-int"
+CACHE_CONTROL="max-age=60, must-revalidate"
+
+aws --profile "$PROFILE"                   \
+    s3 sync                                \
+    "$LOCAL"                               \
+    "$REMOTE"                              \
+    --exclude             "*.DS_Store*"    \
+    --exclude             "*.git*"         \
+    --exclude             "*.gitkeep*"     \
+    --exclude             "*.gitignore*"   \
+    --exclude             "*.svn*"         \
+    --exclude             "*.sh*"          \
+    --acl                 public-read      \
+    --cache-control       "$CACHE_CONTROL" \
+    --no-follow-symlinks                   \
+    --metadata-directive  REPLACE
