314663c2f3cb045370070079bc0a04c7e5ae263c
Bob Dewhirst <bob.dewhirst@bbc.co.uk>
2017-08-15 11:16:12 +0100

    Adding mediaPlayer callbacks

--- src/demo-web-app/www/libs/js/gmi-extensions/media-player.js
+++ src/demo-web-app/www/libs/js/gmi-extensions/media-player.js
@@ -2,7 +2,10 @@ define(function(require) {
     "use strict"
 
     return {
-    	playMedia: function( vpid ) {
+    	playMedia: function( vpid, onMediaPlayerClosed ) {
+
+            window.gmiCallbacks.onMediaPlayerClosed = onMediaPlayerClosed
+
     		if( window.webkit ) {
     			window.webkit.messageHandlers.gmi.postMessage({ "name": "playMedia",  "vpid": vpid })
     		} else {
