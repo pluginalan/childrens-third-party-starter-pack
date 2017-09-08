db6c8dcc739a4126b1edb4b91ace6dec4bdee12e
Emanuel Canha <emanuel.canha@bbc.co.uk>
2017-09-08 13:40:05 +0100

    remove content_old folder

--- src/demo-web-app/bundled/content_old/tools/picknmix-service/limited-storage-run-service.sh
+++
@@ -1,7 +0,0 @@
-# run this script in the folder you want to serve the Playtime Island package from.
-# It sets up routes for;
-# remote/catalogue 			- location to pull catalogue info from
-# remote/catalogue-image	- location to pull catalogue images from, configured separately for flexibility
-
-node picknmix-service.js -x /remote/catalogue-image^https://s3-eu-west-1.amazonaws.com/childrens-data-public-int/apps/cbeebies-playtime-island-dev/data/catalogue/images -x /remote/catalogue^https://s3-eu-west-1.amazonaws.com/childrens-data-public-int/apps/cbeebies-playtime-island-dev/data/catalogue -d 30000000 --packages ./../../fake-packages -s ./../../../webBuild
-
