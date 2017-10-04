a250ef6e7251917b35b3d034cb60838fcbb19418
Johan Jungbeck <johan.jungbeck@bbc.co.uk>
2017-10-04 15:28:47 +0100

    Create Cancel unit test
    Packages should have a cancel function, that can cancel a download.
    This commit  will add unit test framework.

---
+++ test/gmi-extensions/packagesTest.js
@@ -0,0 +1,13 @@
+require("amd-loader");
+
+describe("packages",function () {
+
+    var packages = require("../../src/demo-web-app/pnm-container/pnm/js/gmi-extensions/packages")
+
+    beforeEach(function () {
+
+    });
+    it("Should cancel a download", function () {
+        //expect(packages).toBe(packages)
+    });
+});
