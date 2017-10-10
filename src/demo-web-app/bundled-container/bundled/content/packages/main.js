32fa8dd810326076fad6c821c10aba72a32cbc7f
Bob Dewhirst <bob.dewhirst@bbc.co.uk>
2017-10-10 17:23:00 +0100

    output package buttons

--- src/demo-web-app/bundled-container/bundled/content/packages/main.js
+++ src/demo-web-app/bundled-container/bundled/content/packages/main.js
@@ -30,17 +30,16 @@ define(['pnm-library/gmi-mobile', '../js/ui-helper'], function(gmi_platform, ui_
     ui_helper.appendSubtitle("Available packages");
     var packagesParagraph = ui_helper.appendParagraph();
 
-    gmi.packages.list().then((result) => {
-        result.forEach((aPackage) => {
-            packagesParagraph.appendChild(ui_helper.appendParagraph(JSON.stringify(aPackage)));
-        })
-    })
+
+    gmi.packages.list().then((packages) => {
+        setupView(packages)
+    }, (error) => {
+        console.log("Error: " + error)
+    });
 
     ui_helper.appendHorizontalRule()
 
     // Game loaded
-
-    gmi.gameLoaded();
 
     var popParams = {
         "last_experience": "packages",
@@ -52,5 +51,66 @@ define(['pnm-library/gmi-mobile', '../js/ui-helper'], function(gmi_platform, ui_
         gmi.experiences.pop(popParams);
     });
 
+    gmi.gameLoaded();
 
+    function setupView(packages) {
+        packages.forEach((aPackage) => {
+            var packageContainer = ui_helper.appendDiv()
+            packageContainer.className = "package-title"
+            ui_helper.appendSpan(aPackage.packageId, packageContainer)
+            ui_helper.appendSpan(aPackage.status, packageContainer, "label " + aPackage.status)
+
+            if (aPackage.readOnly) {
+                ui_helper.appendSpan("readOnly", packageContainer, "label read-only")
+            }
+
+            var buttonsContainer = ui_helper.appendDiv()
+            buttonsContainer.appendChild(ui_helper.appendBtn("Download", () => {
+
+                var success = function( resultObject ) {
+                    console.log( "success", resultObject )
+                }
+
+                var failure = function( failureObject ) {
+                    console.log( "failed", failureObject )
+                }
+
+                var pid = aPackage.packageId
+                var pmd = aPackage.metadataObject
+                var purl = aPackage.basepath + aPackage.packageId
+                purl = 
+
+                downloadManager.download( pid, pmd, purl ).then(
+                    success, failure
+                )
+            }))
+
+            buttonsContainer.appendChild(ui_helper.appendBtn("Cancel", () => {
+                console.log("Button clicked")
+            }))
+
+            buttonsContainer.appendChild(ui_helper.appendBtn("Delete", () => {
+                console.log("Button clicked")
+            }))
+
+            var listenerTitle = ui_helper.appendDiv()
+            listenerTitle.className = "package-title"
+            ui_helper.appendSpan("Listeners", listenerTitle)
+
+            var listenerButtons = ui_helper.appendDiv();
+            listenerButtons.appendChild(ui_helper.appendBtn("Add", () => {
+                console.log("Button clicked")
+            }))
+
+            listenerButtons.appendChild(ui_helper.appendBtn("Remove", () => {
+                console.log("Button clicked")
+            }))
+
+            listenerButtons.appendChild(ui_helper.appendBtn("Remove all", () => {
+                console.log("Button clicked")
+            }))
+
+            ui_helper.appendHorizontalRule()
+        })
+    }
 });
